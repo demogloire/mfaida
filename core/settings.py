24d70e91df2865c48265984be77661cd2ebca4ad
Demo8 <chirimwami2014@gmail.com>
2026-05-07 15:15:14 +0100

    fix: retrait init_command MySQL incompatible avec JawsDB

--- core/settings.py
+++ core/settings.py
@@ -92,7 +92,6 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '3306'),
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
             'charset': 'utf8mb4',
         },
     }
