6903286248e50d17d9f91dc1acf36107ed0301c4
Demo8 <chirimwami2014@gmail.com>
2026-05-07 12:39:14 +0100

    feat: logo M-FAIDA, favicon et redirection racine vers connexion

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
-from django.conf.urls.static import static 
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='user:connexion', permanent=False)),
     path('admin/', admin.site.urls),
     path('entreprise/', include('entreprise.urls')),
     path('user/', include('utilisateur.urls')),
